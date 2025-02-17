6413db57de4c1a95388041b9d0d25e32f697fbe5
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2025-02-17 13:14:14 +0200

    Change search term to pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit
 
 # Converting Snowpark dataframe to pandas
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 selected_items = st.multiselect(
     "Choose up to 5 items",
@@ -29,6 +29,8 @@ ingredients_string = ''
 if selected_items:
     for selected_item in selected_items:
         ingredients_string += selected_item + " "
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == selected_item, 'SEARCH_ON'].iloc[0]
+        st.write("The search value for", selected_item, " is ", search_on, ".")
         st.subheader(f"{selected_item} Nutrition information")
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + selected_item)
         sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width = True)
